eb383573b3146748ebe6c1ae41055dd63f3ab4ab
dimitriaoanamaria <dimitria_oana_maria@yahoo.com>
2018-11-26 01:04:08 +0200

    Added functions for correctness check

--- cholesky_pthreads.c
+++ cholesky_pthreads.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 #include <pthread.h>
 
 #define THREADS_NUMBER 2
@@ -13,14 +14,47 @@ void display_matrix(double *A, int n) {
     }
 }
 
-void *cholesky(void *arguments) {
+void *cholesky(void *param) {
     
+}
+
+double * multiply_matrix_with_transpose(double *l, int n) {
+    int i, j, k;
+    double *result = (double *)calloc(n * n, sizeof(double));
+
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            for(k = 0; k < n; k++)
+                result[i * n + j] += l[i * n + k] * l[j * n + k];
+
+    return result;
+}
+
+
+bool check_for_correctness(double *a, double *l, int n, double precision) {
+    int i, j, k;
+    bool ok = true;
+    double *result = multiply_matrix_with_transpose(l, n);
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(abs(a[i*n+j] - result[i*n+j]) > precision){
+                ok = false;
+                break;
+            }
+        }
+    }
+
+    free(result);
+
+    return ok;
 }
 
 int main(int argc, char* argv[]) {
 	FILE *file;
 	int i, j, n;
     double *mat;
+    
     pthread_t threads[THREADS_NUMBER];
 
     if (argc < 2) {
@@ -38,6 +72,8 @@ int main(int argc, char* argv[]) {
     
     /* allocate memory for initial matrix */
     mat = (double *)malloc(n * n * sizeof(double));
+    if(mat == NULL)
+        exit(EXIT_FAILURE);
      
     /* read matrix from file */
     for (i = 0; i < n; i++) {
@@ -62,6 +98,8 @@ int main(int argc, char* argv[]) {
     for (i = 0; i < THREADS_NUMBER; i++) {
         pthread_join(threads[i], NULL);
     }
+
+    display_matrix(mat, n);
 
     return 0;
 }
