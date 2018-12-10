a563066a6e17b53aaee24f07c3e392f72e4c8be1
Theodor291 <teodor.banica1@gmail.com>
2018-12-10 02:37:37 +0200

    Update cholesky_openmp.c

--- cholesky_openmp.c
+++ cholesky_openmp.c
@@ -12,64 +12,26 @@ double *cholesky(double *A, int n) {
     if (L == NULL)
         exit(EXIT_FAILURE);
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (i+1); j++) {
+    for (int j = 0; j <n; j++) {
+        
+        double s = 0;
+        for (int k = 0; k < j; k++) {
+            s += L[j * n + k] * L[j * n + k];
+        }
+        L[j * n + j] = sqrt(A[j * n + j] - s);
+        for (int i = j+1; i <n; i++) {
             double s = 0;
             for (int k = 0; k < j; k++) {
                 s += L[i * n + k] * L[j * n + k];
             }
-            L[i * n + j] = (i == j) ? sqrt(A[i * n + i] - s) : (1.0 / L[j * n + j] * (A[i * n + j] - s));
+            L[i * n + j] = (1.0 / L[j * n + j] * (A[i * n + j] - s));
         }
     }
     return L;
-}
-
-
-double inner_sum(double *li, double *lj, int n) {
-    double s = 0;
-    for (int i = 0; i < n; i++) {
-        s += li[i]*lj[i];
-    }
-    return s;
 }
 
 
 double *cholesky2(double *A, int n) {
-    double *L = (double*)calloc(n * n, sizeof(double));
-    if (L == NULL)
-        exit(EXIT_FAILURE);
-    
-    for (int j = 0; j <n; j++) {
-        #pragma omp parallel for
-        for (int i = j; i <n; i++) {
-            double s = 0;
-            for (int k = 0; k < j; k++) {
-                s += L[i * n + k] * L[j * n + k];
-            }
-            L[i * n + j] = (i == j) ? sqrt(A[i * n + i] - s) : (1.0 / L[j * n + j] * (A[i * n + j] - s));
-        }
-    }
-    return L;
-}
-
-double *cholesky3(double *A, int n) {
-    double *L = (double*)calloc(n * n, sizeof(double));
-    if (L == NULL)
-        exit(EXIT_FAILURE);
-    #pragma omp parallel for
-    for (int j = 0; j <n; j++) {
-        for (int i = j; i <n; i++) {
-            double s = 0;
-            for (int k = 0; k < j; k++) {
-                s += L[i * n + k] * L[j * n + k];
-            }
-            L[i * n + j] = (i == j) ? sqrt(A[i * n + i] - s) : (1.0 / L[j * n + j] * (A[i * n + j] - s));
-        }
-    }
-    return L;
-}
-
-double *cholesky4(double *A, int n) {
     double *L = (double*)calloc(n * n, sizeof(double));
     if (L == NULL)
         exit(EXIT_FAILURE);
@@ -125,7 +87,7 @@ int main() {
     printf("time %f\n", time);
 
     time = omp_get_wtime();
-    double *col2 = cholesky4(matrix, n);
+    double *col2 = cholesky2(matrix, n);
     time = omp_get_wtime() - time;
     printf("time %f\n", time);
 
